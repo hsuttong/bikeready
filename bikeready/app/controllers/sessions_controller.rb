f5f5d6b7ee6345cda314061a84548b3fc4414dc5
derekztang@gmail.com <eskerz117@hotmail.com>
2015-05-29 11:22:20 -0700

    initial log-in page

---
+++ bikeready/app/controllers/sessions_controller.rb
@@ -0,0 +1,7 @@
+class SessionsController < ApplicationController
+  def index
+    @user = User.new
+
+  end
+
+end
--- bikeready/config/routes.rb
+++ bikeready/config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+post 'login' => 'sessions#create'
+   resources :user
+
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'sessions#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -12,7 +15,8 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+
+
 
   # Example resource route with options:
   #   resources :products do
