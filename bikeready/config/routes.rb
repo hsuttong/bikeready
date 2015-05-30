785dce53dcafa4bf26e222b8fe2a41c6607ad0ef
derekztang@gmail.com <eskerz117@hotmail.com>
2015-05-29 18:43:27 -0700

    routes update

--- bikeready/config/routes.rb
+++ bikeready/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   # post 'login' => 'sessions#create'
 
-   resources :users
+  resources :users
 
 
   # You can have the root of your site routed with "root"
