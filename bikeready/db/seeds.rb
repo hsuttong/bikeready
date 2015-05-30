d2f89875f591ccdb0c85878aa8d12acdfd8a5847
Hugh Sutton-Gee <hsuttong@gmail.com>
2015-05-29 21:25:28 -0700

    change seed file

--- bikeready/db/seeds.rb
+++ bikeready/db/seeds.rb
@@ -7,20 +7,29 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'faker'
 
-5.times do
-  user = User.create(first_name: Faker::Name.name,
-                     last_name: Faker::Name.name,
-                     email: Faker::Internet.email,
-                     phone: Faker::PhoneNumber.phone_number,
-                     password: 'password')
-  Address.create(city:     Faker::Address.city,
-                 state:    Faker::Address.state,
-                 street:   Faker::Address.street_address,
-                 zip_code: Faker::Address.zip,
-                 user_id:  user.id)
+# 5.times do
+#   user = User.create(first_name: Faker::Name.name,
+#                      last_name: Faker::Name.name,
+#                      email: Faker::Internet.email,
+#                      phone: Faker::PhoneNumber.phone_number,
+#                      password: 'password')
+#   Address.create(city:     Faker::Address.city,
+#                  state:    Faker::Address.state,
+#                  street:   Faker::Address.street_address,
+#                  zip_code: Faker::Address.zip,
+#                  user_id:  user.id)
 
-  Bike.create(make: 'mountain bike', name: 'lorenzo_bike', color: Faker::Commerce.color, status: 'with_user', user_id: user.id)
-end
+#   Bike.create(make: 'mountain bike', name: 'lorenzo_bike', color: Faker::Commerce.color, status: 'with_user', user_id: user.id)
+# end
 
-user = User.create(first_name: "Hugh", last_name: "Sutton-Gee", email: "hugh@hugh.com", phone: "415-555-1212", password: 'password')
-Bike.create(make: 'mountain bike', name: 'lorenzo_bike', color: Faker::Commerce.color, status: 'with_user', user_id: user.id)
+user1 = User.create(first_name: "Hugh", last_name: "Sutton-Gee", email: "hugh@hugh.com", phone: "415-555-1212", password: 'password')
+user2 = User.create(first_name: "Joyce", last_name: "Delt", email: "joyce@test.com", phone: "415-555-1212", password: 'password')
+user3 = User.create(first_name: "Derek", last_name: "Tang", email: "derek@derek.com", phone: "415-555-1212", password: 'password')
+user4 = User.create(first_name: "Calvin", last_name: "Settachatgul", email: "calvin@calvin.com", phone: "415-555-1212", password: 'password')
+
+bike1 = Bike.create(make: 'mountain bike', name: 'hugh_bike', color: Faker::Commerce.color, status: 'with_user', user_id: 1)
+bike2 = Bike.create(make: 'road bike', name: 'joyce_bike', color: Faker::Commerce.color, status: 'with_user',  user_id: 2)
+bike3 = Bike.create(make: 'touring bike', name: 'derek_bike', color: Faker::Commerce.color, status: 'with_user' ,  user_id: 3)
+bike4 = Bike.create(make: 'race bike', name: 'calvin_bike', color: Faker::Commerce.color, status: 'with_user' ,  user_id: 4)
+
+# Bike.create(make: 'mountain bike', name: 'lorenzo_bike', color: Faker::Commerce.color, status: 'with_user', user_id: user.id)
