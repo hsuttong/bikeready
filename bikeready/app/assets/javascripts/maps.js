9d4b835913137868297b5e74ced429391c93562d
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2015-06-01 15:27:44 -0700

    style show page

--- bikeready/app/assets/javascripts/maps.js
+++ bikeready/app/assets/javascripts/maps.js
@@ -3,7 +3,8 @@
         var myLatlng = new google.maps.LatLng(37.773736, -122.504739);
         var mapOptions = {
           center: { lat: 37.77, lng: -122.419},
-          zoom: 12
+          zoom: 13, 
+          styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}],
         };
         var map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
@@ -42,7 +43,7 @@
           setTimeout(function(coords) {
             route.getPath().push(coords);
             moveMarker(map, marker, coords);
-            }, 200 * i, pathCoords[i]);
+            }, 500 * i, pathCoords[i]);
         }
       }
 
