f68d801ea27da99854ba661a02c4ffd85df82691
Hugh Sutton-Gee <hsuttong@gmail.com>
2015-06-02 18:56:16 -0700

    update google map to use terrain view as default

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -4,6 +4,7 @@
         var mapOptions = {
           center: { lat: 37.77, lng: -122.419},
           zoom: 13, 
+          mapTypeId: google.maps.MapTypeId.TERRAIN,
           styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}],
         };
         var map = new google.maps.Map(document.getElementById('map-canvas'),
