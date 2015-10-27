a094fe9831136bbbcef3aa337ca1fec27816adb5
Hugh Sutton-Gee <hsuttong@gmail.com>
2015-10-27 01:12:22 -0700

    modified:   .ruby-version -Bump ruby version to 2.3.3
    modified:   Gemfile
    modified:   Gemfile.lock
    -Upgrade all gems to latest
    
    modified:   app/controllers/sessions_controller.rb
    -remove breakpoint
    
    modified:   config/initializers/devise.rb
    -remove deprecated device config

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,6 @@ class SessionsController < ApplicationController
       puts "*****************"
       puts "we are in create session"
     @user = User.where(email: params[:email]).first
-    binding.pry
     if @user && @user.authenticate(params[:users][:password_digest])
       session[:id] = @user.id
       puts "*****************"
