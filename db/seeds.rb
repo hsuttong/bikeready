75b750c31bdb4fd3ee3732dc61fe8a3ca61f2411
Hugh Sutton-Gee <hsuttong@gmail.com>
2016-08-13 16:12:33 -0700

    Update gems, clean-up seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,28 +12,21 @@ user2 = User.create(first_name: "Joyce", last_name: "Delatore", email: "joycedel
 user3 = User.create(first_name: "Derek", last_name: "Tang", email: "derekztang@gmail.com", phone: "4158121901", password: 'password')
 user4 = User.create(first_name: "Calvin", last_name: "Settachatgul", email: "calvin.settachatgul@gmail.com", phone: "5102208405", password: 'password')
 
-bike1 = Bike.create(make: 'mountain bike', name: 'hugh_bike', color: Faker::Commerce.color, status: 'storage', user_id: 1)
-bike2 = Bike.create(make: 'road bike', name: 'joyce_bike', color: Faker::Commerce.color, status: 'storage',  user_id: 2)
-bike3 = Bike.create(make: 'touring bike', name: 'derek_bike', color: Faker::Commerce.color, status: 'storage' ,  user_id: 3)
-bike4 = Bike.create(make: 'race bike', name: 'calvin_bike', color: Faker::Commerce.color, status: 'storage' ,  user_id: 4)
+user1.bikes.create(make: 'mountain bike', name: 'hugh_bike', color: Faker::Commerce.color, status: 'storage')
+user2.bikes.create(make: 'road bike', name: 'joyce_bike', color: Faker::Commerce.color, status: 'storage')
+user3.bikes.create(make: 'touring bike', name: 'derek_bike', color: Faker::Commerce.color, status: 'storage')
+user4.bikes.create(make: 'race bike', name: 'calvin_bike', color: Faker::Commerce.color, status: 'storage')
 
 (2..4).each do |i|
-#   user = User.create(first_name: Faker::Name.name,
-#                      last_name: Faker::Name.name,
-#                      email: Faker::Internet.email,
-#                      phone: Faker::PhoneNumber.phone_number,
-#                      password: 'password')
    Address.create(street:   Faker::Address.street_address,
                   city:     Faker::Address.city,
                   state:    Faker::Address.state,
                   zip_code: Faker::Address.zip,
                   user_id:  i)
-
-#   Bike.create(make: 'mountain bike', name: 'lorenzo_bike', color: Faker::Commerce.color, status: 'storage', user_id: user.id)
 end
 
-   Address.create(street:   '33 Montclair Terrace',
-                  city:     'San Francisco',
-                  state:    'CA',
-                  zip_code: '94109',
-                  user_id:  1)
+Address.create( street:   '33 Montclair Terrace',
+                city:     'San Francisco',
+                state:    'CA',
+                zip_code: '94109',
+                user_id:  1)
